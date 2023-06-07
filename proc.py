4fd4c8d29adcf553d976dd3dcbe593b44c665fb8
Swiatique <s.vlasov@rqc.ru>
2023-06-07 19:25:03 +0300

    Add run function hardware implementation

--- proc.py
+++ proc.py
@@ -1,9 +1,12 @@
+from time import time
 from ui import BurnerSetupWindow
 from PyQt6.QtWidgets import QMessageBox
 import asyncio
 
 from hardware import DDS220M, PowerPlot, StandaMotor, VControl
 from hardware.standa import initDevices as initStandaMotors
+
+from misc import getLx
 
 lock = asyncio.Lock()
 
@@ -19,6 +22,10 @@ burnerMotor0 = 10
 burnerMotor1 = 36.8
 pullingMotor1_0 = 0
 pullingMotor2_0 = 0
+
+ts = []
+xs = []
+Ls = []
 
 def initDevices():
     global mainMotor, burnerMotor, pullingMotor1, pullingMotor2, vControl, powerPlot
@@ -127,3 +134,40 @@ async def extinguish():
         await vControl.extinguish()
         if repeatition.exec() == QMessageBox.StandardButton.No: # check if extinguished
             break   
+
+def _getX():
+    xr0 = pullingMotor1_0 + pullingMotor2_0
+    xr = pullingMotor1.getPosition() + pullingMotor2.getPosition()
+    return xr - xr0
+
+async def _mainMotorRun(Lx, xMax):
+    turn = 1
+    while True:
+        x = _getX()
+        if x >= xMax:
+            break
+        yTarget = Lx(x) * turn
+        ts.append(time())
+        xs.append(x)
+        Ls.append(yTarget * turn)
+        await mainMotor.moveTo(yTarget)
+        turn *= -1
+
+async def _pullerMotorRun(xMax):
+    await asyncio.gather(pullingMotor1.moveTo(pullingMotor1_0 + xMax / 2),
+                         pullingMotor2.moveTo(pullingMotor2_0 + xMax / 2))
+
+async def run(rw=20, lw=30, r0=62.5, dr=1, tWarmen=0):
+    Lx, Rx, xMax, _, _ = getLx(r0=r0, rw=rw, lw=lw, dr=dr)
+
+    await burnerMotor.moveTo(burnerMotor1) # Подвод горелки
+    t0 = time() # Время начала прогрева
+
+    mainMotorTask = asyncio.create_task(_mainMotorRun(Lx, xMax))
+    while time() - t0 < tWarmen:
+        await asyncio.sleep(0)
+    
+    await _pullerMotorRun(xMax)
+    mainMotorTask.cancel()
+
+    
