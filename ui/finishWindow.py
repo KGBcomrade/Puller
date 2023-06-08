e68edf12408f8467f7741aeb8b370572eca93ef0
Swiatique <s.vlasov@rqc.ru>
2023-06-08 16:24:23 +0300

    Replace moveButton with moveCheckBox in FinishWindow

--- ui/finishWindow.py
+++ ui/finishWindow.py
@@ -5,43 +5,32 @@ from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow, QVB
 from PyQt6.QtCore import QSize, Qt, pyqtSignal, QLocale
 
 class FinishWindow(QDialog):
-    def __init__(self, stretchFunction: typing.Callable, moveFuntion: typing.Callable) -> None:
+    def __init__(self, stretchFunction: typing.Callable) -> None:
         super().__init__()
 
         self._stretchFunction = stretchFunction
-        self._moveFunction = moveFuntion
 
         mainLayout = QVBoxLayout()
         self.setLayout(mainLayout)
 
         self.saveCheckBox = QCheckBox('Сохранить данные')
+        self.moveCheckBox = QCheckBox('Сместить раму вправо')
         self.stretchButton = QPushButton('Доп. растяжка')
-        self.moveButton = QPushButton('Переместить раму вправо')
         self.exitButton = QPushButton('ОК')
         
         mainLayout.addWidget(self.saveCheckBox)
+        mainLayout.addWidget(self.moveCheckBox)
         mainLayout.addWidget(self.stretchButton)
-        mainLayout.addWidget(self.moveButton)
         mainLayout.addWidget(self.exitButton)
+
+        self.stretchButton.released.connect(self._stretch)
+        self.exitButton.released.connect(self.accept)
 
     def _setButtonsEnabled(self, enabled):
         self.stretchButton.setEnabled(enabled)
-        self.moveButton.setEnabled(enabled)
         self.exitButton.setEnabled(enabled)
 
     def _stretch(self):
         self._setButtonsEnabled(False)
         self._stretchFunction()
         self._setButtonsEnabled(True)
-
-    def _move(self):
-        self._setButtonsEnabled(False)
-        self._moveFunction()
-        self.exitButton.setEnabled(True)
-
-    def _exit(self):
-        if self.saveCheckBox.isChecked():
-            #TODO save data
-            pass
-
-        self.accept()
