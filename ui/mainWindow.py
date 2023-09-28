b90e6b9a35c3aa20731a3f4c6dcc41060e6725fa
Swiatique <s.vlasov@rqc.ru>
2023-09-28 14:26:03 +0300

    Update twarmen

--- ui/mainWindow.py
+++ ui/mainWindow.py
@@ -234,7 +234,7 @@ class MainWindow(QMainWindow):
         self.startButton.setText(startButtonStopText)
         self.startButton.released.disconnect()
         self.startButton.released.connect(self.callStop)
-        await self.proc.run(self, self.rw, self.lw, self.r0, tWarmen=1)
+        await self.proc.run(self, self.rw, self.lw, self.r0, tWarmen=35)
 
     def callStop(self):
         self.startButton.setEnabled(False)
